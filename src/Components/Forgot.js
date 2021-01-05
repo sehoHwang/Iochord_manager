0661a6e22ca71b8d67586e0ae5fea0ecc9331b57
seho <seho4815@gmail.com>
2021-01-05 19:43:47 +0900

    Axios with express

--- src/Components/Forgot.js
+++ src/Components/Forgot.js
@@ -2,18 +2,49 @@ import React from 'react';
 import {Button, Form} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
+import axios from 'axios';
+
+var current = '';
 
 class Forgot extends React.Component{
+
+    state={
+        id: '',
+    }
+
+    handleChange = (event) => {
+        this.setState({id: event.target.value});
+        console.log(this.state.id);
+    }
+
+    handleSubmit = (id) => {
+        current = this.state.id;
+        this.pushPassword();
+    }
+
+    pushPassword = async() => {
+        //const { id } = this.state.id;
+        console.log(this.state.id);
+        await axios.get('http://172.26.127.17:5000/password?id='+current)
+        .then(res => {
+            
+        })
+        
+    }
+
     render(){
         return(
             <div className = "Form">
                 <Form>
                     <Form.Group>
                         <Form.Label>Administor ID</Form.Label>
-                        <Form.Control placeholder="Input your ID"/>
-                        <Form.Text>귀하의 계정을 입력해주세요</Form.Text>
+                        <Form.Control placeholder="Input your ID"
+                            defaultValue={this.state.id}
+                            onChange={this.handleChange}
+                        />
+                        <Form.Text >귀하의 계정을 입력해주세요</Form.Text>
                     </Form.Group>
-                    <div style={{textAlign:'center', marginBottom:20}}><Button variant="secondary" type="submit">Find</Button></div>
+                    <div style={{textAlign:'center', marginBottom:20}}><Button variant="secondary" type="submit" onClick={()=>this.handleSubmit(this.state.id)}>Find</Button></div>
                 </Form>
             </div>
         );
--- src/Components/Update.js
+++ src/Components/Update.js
@@ -4,6 +4,24 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 
 class Update extends React.Component{
+
+    state={
+        id: '',
+        password: '',
+    }
+
+    handleID = (event) => {
+        this.setState({id: event.target.value});
+    }
+
+    handlePassword = (event) => {
+        this.setState({password: event.target.value});
+    }
+
+    handleSubmit = () => {
+        alert(this.state.id + this.state.password);
+    }
+
     render(){
         return(
             
@@ -11,15 +29,30 @@ class Update extends React.Component{
                 <Form>
                     <Form.Group>
                         <Form.Label>Administor ID</Form.Label>
-                        <Form.Control placeholder="Input your ID"/>
+                        <Form.Control
+                            placeholder="Input your ID"
+                            defaultValue={this.state.id}
+                            onChange={this.handleID}
+                         />
                         <Form.Text>귀하의 계정을 입력해주세요</Form.Text>
                     </Form.Group>
                     <Form.Group>
                         <Form.Label>Administor Password</Form.Label>
-                        <Form.Control type="password" placeholder="Input your Password"/>
+                        <Form.Control
+                            type="password"
+                            placeholder="Input your Password"
+                            defaultValue={this.state.password}
+                            onChange={this.handlePassword}
+                            />
                         <Form.Text>귀하의 비밀번호를 입력해주세요</Form.Text>
                     </Form.Group>
-                    <div style={{textAlign:'center', marginBottom:20}}><Button variant="secondary" type="submit">Reset</Button></div>
+                    <div style={{textAlign:'center', marginBottom:20}}>
+                        <Button variant="secondary" type="submit"
+                            onClick={() => this.handleSubmit()}
+                        >
+                            Reset
+                        </Button>
+                    </div>
                 </Form>
             </div>
                
